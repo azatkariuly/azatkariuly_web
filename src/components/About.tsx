b24478c7382aa25bf8e6d3c0c8fb64df04f31f69
Azat Azamat <azatkariuly@Azats-MacBook-Pro.local>
2024-10-14 18:55:09 +0900

    test

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,7 +18,7 @@ const About = () => {
         <div className="w-[calc((100vw)/2)] h-[calc((100vw)/2)] md:w-1/3 md:h-[calc((100vw-64px)/3)] relative">
           <Image
             src={'/avatarr.jpg'}
-            alt="Picture of the author"
+            alt="Avatar"
             layout="fill" // required
             objectFit="cover" // change to suit your needs
             // className="rounded-xl md:rounded-full"
