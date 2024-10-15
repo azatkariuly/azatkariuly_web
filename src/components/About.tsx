e9b48fb7afbbc8650bd39ab5045d43686836eb5b
Azat Azamat <azatkariuly@Azats-MacBook-Pro.local>
2024-10-15 23:25:38 +0900

    update: about photo

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,6 +11,15 @@ const About = () => {
       <div className='page-title'>ABOUT ME</div>
   
       <div className='flex flex-col md:flex-row items-center justify-center gap-4'>
+        <div className="w-[calc((100vw)/2)] h-[calc((100vw)/2)] md:w-1/3 md:h-[calc((100vw-64px)/3)] relative">
+          <Image
+            src={'/avatarr.jpg'}
+            alt="Avatar"
+            layout="fill" // required
+            objectFit="cover" // change to suit your needs
+            // className="rounded-xl md:rounded-full"
+          />
+        </div>
         <div className='flex flex-col md:w-2/3 text-lg md:textxl pr-10'>
           <div>My name is Azat Azamat</div>
           <div>
@@ -43,15 +52,6 @@ const About = () => {
             </Link>
             
           </div>
-        </div>
-        <div className="w-[calc((100vw)/2)] h-[calc((100vw)/2)] md:w-1/3 md:h-[calc((100vw-64px)/3)] relative">
-          <Image
-            src={'/avatar.jpg'}
-            alt="Avatar"
-            layout="fill" // required
-            objectFit="cover" // change to suit your needs
-            // className="rounded-xl md:rounded-full"
-          />
         </div>
       </div>
     </div>
